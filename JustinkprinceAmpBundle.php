526dee0acf36ad726909585e297ec0f7534cb6d7
Justin Prince <justin.prince@cbsinteractive.com>
2016-03-05 15:51:37 -0500

    Added basic bundle structure.

---
+++ JustinkprinceAmpBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Justinkprince\Bundle\AmpBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JustinkprinceAmpBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
